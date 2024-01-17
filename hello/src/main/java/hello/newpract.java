984120d300a8c1931328b89a3e44a88222b58f33
hariprasad <hariprasadnomula7@gmail.com>
2024-01-17 23:00:39 +0530

    hello

---
+++ hello/src/main/java/hello/newpract.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class newpract {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("created by hari");
+
+	}
+
+}
